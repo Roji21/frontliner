2e427cb30d07f593ed6ebaa442a73239f7a21f16
Mukhamad Rozi <116157556+Roji21@users.noreply.github.com>
2023-06-20 11:09:26 +0700

    hapus

--- app/Views/faq.php
+++
@@ -1,8 +0,0 @@
-<?= $this->extend('layout/page') ?>
-
-<?= $this->section('content') ?>
-<div class="container">
-    <h2 class="p-3">Ini halaman FAQs Browwww</h2>
-</div>
-
-<?= $this->endSection() ?>
